9f8dd00f2fc5425683e635a48bb372f1bcad3694
Arthur Trindade <thucatrindade1@gmail.com>
2025-08-21 15:49:32 -0300

    Made home non-absolutely positioned and made all selectors into a v-for statement

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,12 +2,10 @@
 import { ChevronsDown } from 'lucide-vue-next';
 import { ref, computed, defineComponent, h } from 'vue';
 
-type ContentType = "Academic" | "Professional" | "Artistic" | "";
-const selected = ref<ContentType>("");
+type Category = "Academic" | "Professional" | "Artistic";
+const selected = ref<Category | "">("");
 
-const basePanelClass = 'flex items-center justify-center h-full basis-0 min-w-0 overflow-hidden transition-[flex-grow] duration-300 select-none';
-
-const growClass = (name : ContentType) =>
+const growClass = (name : Category) =>
   selected.value === ''
     ? 'grow hover:grow-[1.5]'
     : (selected.value === name ? 'grow' : 'grow-0')
@@ -25,7 +23,7 @@ const   ContentWrapper = defineComponent({
         'div',
         {
           class: [
-            'transition-opacity pointer-events-none select-none duration-300',
+            'transition-opacity pointer-events-none select-none duration-500',
             opacityClass.value,
           ], 'aria-hidden': !props.selected,
         },
@@ -34,58 +32,35 @@ const   ContentWrapper = defineComponent({
   },
 })
 
+const categoryBg: Record<Category, string> = {
+  Academic: 'bg-red-600',
+  Professional: 'bg-green-600',
+  Artistic: 'bg-blue-600'
+}
 </script>
 
 <template>
-  <div class="absolute bg-black inset-0 w-full h-full flex flex-row items-stretch justify-center cursor-pointer">
-    <div
+  <main class="relative overflow-visible bg-black w-[100vw] flex flex-col items-stretch justify-center">
+    
+    <div class="z-10 h-screen flex items-stretch">
+      <div v-for="category in ['Academic', 'Professional', 'Artistic']" :key="category"
       :class="[
-        basePanelClass,
-        'bg-red-500',
-        growClass('Academic')
+        ' flex items-center justify-center h-full basis-0 min-w-0 overflow-hidden transition-[flex-grow] duration-300 select-none cursor-pointer',
+        growClass(category as Category), categoryBg[category as Category]
       ]"
-      @click="selected === 'Academic' ? selected = '' : selected = 'Academic'"
-      role="button"
-      tabindex="0"
-    >
-      <ContentWrapper :selected="selected === '' || selected === 'Academic'">
-        <h1>Academic</h1>
-      </ContentWrapper>
-    </div>
-    <div
-      :class="[
-        basePanelClass,
-        'bg-green-500',
-        growClass('Professional')
-      ]"
-      @click="selected === 'Professional' ? selected = '' : selected = 'Professional'"
-      role="button"
-      tabindex="1"
-    >
-      <ContentWrapper :selected="selected === '' || selected === 'Professional'">
-        <h1>Professional</h1>
-      </ContentWrapper>
-    </div>
-    <div
-      :class="[
-        basePanelClass,
-        'bg-blue-500',
-        growClass('Artistic')
-      ]"
-      @click="selected === 'Artistic' ? selected = '' : selected = 'Artistic'"
-      role="button"
-      tabindex="2"
-    >
-      <ContentWrapper :selected="selected === '' || selected === 'Artistic'">
-        <h1>Artistic</h1>
-      </ContentWrapper>
+      @click="selected === category ? selected = '' : selected = (category as Category)"
+      >
+        <ContentWrapper :selected="selected === '' || selected === category">
+          <h1>{{category}}</h1>
+        </ContentWrapper>
+      </div>
     </div>
 
-    <span class="absolute bottom-2 flex flex-col items-center">
-      <!-- <ChevronsDown class="w-16 h-16 animate-bounce " /> -->
+    <!-- <span class="absolute bottom-2 flex flex-col items-center">
+      <ChevronsDown class="w-16 h-16 animate-bounce " />
       <p class="text-lg font-semibold">Scroll for Overview</p>
-    </span>
-  </div>
+    </span> -->
+  </main>
 
-  <div class="flex w-full h-full bg-white z-10">ABC</div>
+  <footer class="bg-white"><p class="text-slate-900">©2025 Arthur Trindade</p></footer>
 </template>
