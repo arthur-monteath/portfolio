8641aa0986438bae7bf1bd9708bd43f1ea340652
Arthur Trindade <thucatrindade1@gmail.com>
2025-08-21 02:35:18 -0300

    Initial commit

---
+++ src/pages/About.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import OrbitCarousel from '../components/OrbitCarousel.vue'
+</script>
+
+<template>
+  <div class="bg-black min-h-screen absolute top-0 left-0">
+    <OrbitCarousel />
+  </div>
+</template>
---
+++ src/pages/Home.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import AboutCard from '../components/AboutCard.vue';
+
+</script>
+
+<template>
+  <div class="">
+    <h1 class="text-3xl font-bold text-blue-500">Home Page</h1>
+    <router-link to="/about" class="text-blue-700 underline">Go to About</router-link>
+    <AboutCard />
+  </div>
+</template>
